b01815151fd900dfff4d16dcddd521af00c2e018
debasish mandal <debasishmandal186@gmail.com>
2024-03-21 18:21:37 +0530

    the counter app with redux

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -1,28 +1,48 @@
 import React from 'react'
 
 
-import { UseDispatch,useSelector } from 'react-redux'
+import { UseDispatch,useDispatch,useSelector } from 'react-redux'
 
 
-
+import { increase,decrease,reset } from '../redux/actions/counterActions';
 
 
 
 const CounterApp = () => {
+  // useSelector ---(redux to app) Data Exchange
+  // useDispatch ---(app to redux) Data Exchange
  const count= useSelector(state=>state);
 
+// useDispatch also returns a function 
 
+const dispatch= useDispatch();
+
+
+function handleIncrease (){
+  // call the increase function 
+  dispatch(increase());
+}
+function handleDecrease() {
+  // call the decrease function
+  dispatch(decrease());
+}
+function handleReset() {
+  // call the increase function
+  dispatch(reset());
+}
 
 
   return (
     <div>
-<h1>{count}</h1>
-<button>Increase</button>
+      <h1>{count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+
+      <button onClick={handleReset}>Reset</button>
     </div>
-  )
+  );
 }
 
 export default CounterApp;
 
 
-2:0000
